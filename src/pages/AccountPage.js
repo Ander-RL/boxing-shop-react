c99f8473bba4e164f2527d254daffd48b7b09edf
Ander-RL <anderrodriguez205@gmail.com>
2023-12-15 11:03:23 +0100

    AccountPage info panel

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -18,31 +18,46 @@ const AccountPage = () => {
 
                                 <div className="card border-dark rounded-0">
                                     <div className="card-body">
-                                        <h5 className="card-title">Account information</h5><i class="bi bi-caret-right-fill"></i>
+                                        <div className="row">
+                                            <div className="col"><h5 className="card-title">Account information</h5></div>
+                                            <div className="col-2"><i class="bi bi-caret-right-fill"></i></div>
+                                        </div>
                                     </div>
                                 </div>
 
                                 <div className="card rounded-0">
                                     <div className="card-body">
-                                        <h5 className="card-title">My orders</h5><i class="bi bi-caret-right-fill"></i>
+                                        <div className="row">
+                                            <div className="col"><h5 className="card-title">My orders</h5></div>
+                                            <div className="col-2"><i class="bi bi-caret-right-fill"></i></div>
+                                        </div>
                                     </div>
                                 </div>
 
                                 <div className="card rounded-0">
                                     <div className="card-body">
-                                        <h5 className="card-title">Address book</h5><i class="bi bi-caret-right-fill"></i>
+                                        <div className="row">
+                                            <div className="col"><h5 className="card-title">Address book</h5></div>
+                                            <div className="col-2"><i class="bi bi-caret-right-fill"></i></div>
+                                        </div>
                                     </div>
                                 </div>
 
                                 <div className="card rounded-0">
                                     <div className="card-body">
-                                        <h5 className="card-title">Wishlist</h5><i class="bi bi-caret-right-fill"></i>
+                                        <div className="row">
+                                            <div className="col"><h5 className="card-title">Wishlist</h5></div>
+                                            <div className="col-2"><i class="bi bi-caret-right-fill"></i></div>
+                                        </div>
                                     </div>
                                 </div>
 
                                 <div className="card rounded-0">
                                     <div className="card-body">
-                                        <h5 className="card-title">Change password</h5><i class="bi bi-caret-right-fill"></i>
+                                        <div className="row">
+                                            <div className="col"><h5 className="card-title">Change password</h5></div>
+                                            <div className="col-2"><i class="bi bi-caret-right-fill"></i></div>
+                                        </div>
                                     </div>
                                 </div>
 
@@ -53,38 +68,30 @@ const AccountPage = () => {
                     </div>
                     <div className="col-lg-10 col-md-10 col-sm-10">
 
-                        <div className="card m-1 w-100">
-
-                            <div className="card border-dark rounded-0">
-                                <div className="card-body">
-                                    <h5 className="card-title">Account information</h5><i class="bi bi-caret-right-fill"></i>
+                        <div className="card m-1 w-100 border-black">
+                            <div className="card-body">
+                                <h5 className="card-title">Account information</h5>
+                                <hr className="bg-dark border-2 border-top border-black" />
+                                <div className="row">
+                                    <div className="col"><h6 className="fw-bold">First name</h6></div>
+                                    <div className="col-10"><h6>Johnny</h6></div>
+                                </div>
+                                <hr className="bg-dark border-2 border-top border-black" />
+                                <div className="row">
+                                    <div className="col"><h6 className="fw-bold">Second name</h6></div>
+                                    <div className="col-10"><h6>Silverhand</h6></div>
+                                </div>
+                                <hr className="bg-dark border-2 border-top border-black" />
+                                <div className="row">
+                                    <div className="col"><h6 className="fw-bold">Email</h6></div>
+                                    <div className="col-10"><h6>JohnnyRocks@arasaka.com</h6></div>
+                                </div>
+                                <hr className="bg-dark border-2 border-top border-black" />
+                                <div className="row">
+                                    <div className="col"><h6 className="fw-bold">Birthdate</h6></div>
+                                    <div className="col-10"><h6>01/01/2077</h6></div>
                                 </div>
                             </div>
-
-                            <div className="card rounded-0">
-                                <div className="card-body">
-                                    <h5 className="card-title">My orders</h5><i class="bi bi-caret-right-fill"></i>
-                                </div>
-                            </div>
-
-                            <div className="card rounded-0">
-                                <div className="card-body">
-                                    <h5 className="card-title">Address book</h5><i class="bi bi-caret-right-fill"></i>
-                                </div>
-                            </div>
-
-                            <div className="card rounded-0">
-                                <div className="card-body">
-                                    <h5 className="card-title">Wishlist</h5><i class="bi bi-caret-right-fill"></i>
-                                </div>
-                            </div>
-
-                            <div className="card rounded-0">
-                                <div className="card-body">
-                                    <h5 className="card-title">Change password</h5><i class="bi bi-caret-right-fill"></i>
-                                </div>
-                            </div>
-
                         </div>
 
                     </div>
