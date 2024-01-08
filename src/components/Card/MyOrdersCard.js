ec352dfb4b3e798f57f4618b0e4d09c607b9f724
Ander-RL <anderrodriguez205@gmail.com>
2024-01-08 14:50:10 +0100

    Recovers orders from client, needs new card for product

--- src/components/Card/MyOrdersCard.js
+++ src/components/Card/MyOrdersCard.js
@@ -1,8 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const AddressBookCard = (props) => {
+import ProductCard from "./ProductCard";
 
-    //const { id, title, price, totalPrice, img, description, quantity } = props;
+const AddressBookCard = () => {
+
+    const [idList, setIdList] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchOrdersData();
+    }, []);
+
+    useEffect(() => {
+        fetchProductDataById(idList);
+    }, [idList]);
+
+    function fetchOrdersData() {
+        fetch('http://localhost:8080/react/v1/orders',
+            {
+                method: 'GET',
+                mode: 'cors'
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                setIdList(getOrderIds(data));
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    function fetchProductDataById(productsId) {
+        fetch('http://localhost:8080/react/v1/products/selectedProductsById',
+            {
+                method: 'POST',
+                mode: 'cors',
+                body: JSON.stringify(productsId),
+                headers: {
+                    'Accept': 'application/json, text/plain',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setProductList(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }
+
+    function getOrderIds(order) {
+        var idList = [];
+        order.forEach(product => {
+            idList.push(product.idProduct);
+        });
+
+        return idList;
+    };
+
+    const setProductList = (orderItems) => {
+        let list = [];
+        orderItems.forEach(item => list.push(
+            <ProductCard
+                key={item.id}
+                id={item.id}
+                img={`/img/${item.img}`}
+                title={item.name}
+                description={item.description}
+                price={item.price}
+            />
+        ));
+
+        setProducts(list);
+    };
 
     return (
         <div className="card m-1 w-100 border-black">
@@ -15,6 +87,8 @@ const AddressBookCard = (props) => {
                         <h5 className="card-title">My Order Nº1</h5>
                     </div>
                 </div>
+
+                {products}
 
             </div>
         </div>
