abbac6602b5d58ae91526600541036ae85f65578
Ander-RL <anderrodriguez205@gmail.com>
2024-01-21 15:47:43 +0100

    Text alignment in myOrders card

--- src/components/Card/MyOrdersProductCard.js
+++ src/components/Card/MyOrdersProductCard.js
@@ -24,12 +24,27 @@ const MyOrdersProductCard = (props) => {
         <div className="card text-bg-light m-2 shadow p-3 bg-white rounded" style={{ maxWidth: '18rem' }} key={props.id}>
             <img src={props.img} className="card-img-top" alt="" />
             <div className="card-body d-flex flex-column">
-            
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text fw-light">{props.description}</p>
-                <p className="card-text">{props.price}€</p>
-                <p className="card-text">Quantity</p>
-                <p className="card-text">Total Price</p>
+
+                <div className="card-body  bg-body-tertiary rounded mb-3">
+                    <h5 className="card-title align-self-center">{props.title}</h5>
+                    <span className="card-text fw-light pb-4 align-self-center">{props.description}</span>
+                </div>
+
+                <div className="card-body bg-body-tertiary rounded mb-3 text-center">
+                    <div className="row ">
+                        <span className="badge bg-secondary rounded-pill">Price</span>
+                        <span className="card-text">{props.price}€</span>
+                    </div>
+                    <div className="row">
+                        <span className="badge bg-secondary rounded-pill">Quantity</span>
+                        <span className="card-text">XX</span>
+                    </div>
+                    <div className="row">
+                        <span className="badge bg-secondary rounded-pill">Total Price</span>
+                        <span className="card-text">XX</span>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
